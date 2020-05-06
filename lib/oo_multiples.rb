221c74b80604d2bbfef865b70e0a27cb885575e2
marshall405 <marshall.slemp@gmail.com>
2020-05-06 14:14:02 -0500

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,24 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+    def initialize(limit)
+        @limit = limit
+    end
+
+    def collect_multiples
+        ar = []
+        m = 1
+        while m < @limit
+            if m % 3 == 0 || m % 5 == 0
+                ar << m 
+            end
+            m += 1
+        end
+        ar
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+
+end
